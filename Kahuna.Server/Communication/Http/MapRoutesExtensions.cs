375b90057d3d6ea03bcf541090af3bf20c6d6987
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2025-02-26 08:55:05 -0500

    Upgraded to Kommander 0.1.2

--- Kahuna.Server/Communication/Http/MapRoutesExtensions.cs
+++ Kahuna.Server/Communication/Http/MapRoutesExtensions.cs
@@ -24,16 +24,16 @@ public static class MapRoutesExtensions
             
             // Console.WriteLine("LOCK {0} {1} {2}", request.LockName, request.LockId, request.ExpiresMs);
             
-            int partitionId = (int)raft.GetPartitionKey(request.LockName);
+            int partitionId = raft.GetPartitionKey(request.LockName);
 
-            if (!raft.Joined || await raft.AmILeader(partitionId))
+            if (!raft.Joined || await raft.AmILeader(partitionId, CancellationToken.None))
             {
                 (LockResponseType response, long fencingToken) = await locks.TryLock(request.LockName, request.LockId, request.ExpiresMs, request.Consistency);
 
                 return new() { Type = response, FencingToken = fencingToken };    
             }
             
-            string leader = await raft.WaitForLeader(partitionId);
+            string leader = await raft.WaitForLeader(partitionId, CancellationToken.None);
             if (leader == raft.GetLocalEndpoint())
                 throw new RaftException("lol");
             
@@ -71,16 +71,16 @@ public static class MapRoutesExtensions
             if (request.ExpiresMs <= 0)
                 return new() { Type = LockResponseType.Errored };
             
-            int partitionId = (int)raft.GetPartitionKey(request.LockName);
+            int partitionId = raft.GetPartitionKey(request.LockName);
             
-            if (!raft.Joined || await raft.AmILeader(partitionId))
+            if (!raft.Joined || await raft.AmILeader(partitionId, CancellationToken.None))
             {
                 LockResponseType response = await locks.TryExtendLock(request.LockName, request.LockId, request.ExpiresMs, request.Consistency);
 
                 return new() { Type = response };    
             }
             
-            string leader = await raft.WaitForLeader(partitionId);
+            string leader = await raft.WaitForLeader(partitionId, CancellationToken.None);
             if (leader == raft.GetLocalEndpoint())
                 throw new RaftException("lol");
             
@@ -115,16 +115,16 @@ public static class MapRoutesExtensions
             if (string.IsNullOrEmpty(request.LockId))
                 return new() { Type = LockResponseType.Errored };
             
-            int partitionId = (int)raft.GetPartitionKey(request.LockName);
+            int partitionId = raft.GetPartitionKey(request.LockName);
 
-            if (!raft.Joined || await raft.AmILeader(partitionId))
+            if (!raft.Joined || await raft.AmILeader(partitionId, CancellationToken.None))
             {
                 LockResponseType response = await locks.TryUnlock(request.LockName, request.LockId, request.Consistency);
 
                 return new() { Type = response };
             }
             
-            string leader = await raft.WaitForLeader(partitionId);
+            string leader = await raft.WaitForLeader(partitionId, CancellationToken.None);
             if (leader == raft.GetLocalEndpoint())
                 throw new RaftException("lol");
             
@@ -156,9 +156,9 @@ public static class MapRoutesExtensions
             if (string.IsNullOrEmpty(request.LockName))
                 return new() { Type = LockResponseType.Errored };
             
-            int partitionId = (int)raft.GetPartitionKey(request.LockName);
+            int partitionId = raft.GetPartitionKey(request.LockName);
 
-            if (!raft.Joined || await raft.AmILeader(partitionId))
+            if (!raft.Joined || await raft.AmILeader(partitionId, CancellationToken.None))
             {
                 (LockResponseType response, ReadOnlyLockContext? context) = await locks.GetLock(request.LockName, request.Consistency);
 
@@ -174,7 +174,7 @@ public static class MapRoutesExtensions
                 return new() { Type = response };
             }
             
-            string leader = await raft.WaitForLeader(partitionId);
+            string leader = await raft.WaitForLeader(partitionId, CancellationToken.None);
             if (leader == raft.GetLocalEndpoint())
                 throw new RaftException("lol");
             
