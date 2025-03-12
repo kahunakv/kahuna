8ef26ffefacd06012fbd9d737d444d8e0dc732df
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2025-03-12 13:38:52 -0500

    Try to run tests in Github Actions (6)

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd ..
+
+sudo apt-get install -y ca-certificates
+sudo cp certs/development-certificate.crt /usr/local/share/ca-certificates
+sudo update-ca-certificates
+
 docker build -f DockerfileBase  -t kahuna-base --progress=plain .
 docker compose up --build -d
